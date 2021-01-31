d7ef1bc1873698711c781309abc56671c7902c2b
redmaner <jakevdputten@protonmail.com>
2021-01-31 19:29:50 +0100

    handler: Add helper functions to reply to a Websocket frame or info

--- lib/plug_websocket/handler.ex
+++ lib/plug_websocket/handler.ex
@@ -1,4 +1,8 @@
 defmodule PlugWebsocket.Handler do
+  @moduledoc """
+  `PlugWebsocket.Handler` provides various abstractions to handle Websocket frames received from clients.
+  """
+
   alias PlugWebsocket.Core.{Conn, Frame}
 
   defmacro __using__(opts) do
@@ -17,6 +21,7 @@ defmodule PlugWebsocket.Handler do
 
     quote do
       require Logger
+      import PlugWebsocket.Handler
       @behaviour :cowboy_websocket
       @behaviour PlugWebsocket.Handler
 
@@ -78,9 +83,53 @@ defmodule PlugWebsocket.Handler do
     end
   end
 
+  @doc """
+  `handle_frame` is a callback function to handle Websocket frames. This function receives
+  a Websocket frame, and a websocket connection. It should return a connection reply.
+  """
   @callback handle_frame(frame :: Frame.t(), conn :: Conn.t()) :: Conn.reply()
 
+  @doc """
+  `handle_frame` is a callback function to handle info messages, similar to `GenServer` handle_info callbacks.
+  """
   @callback handle_info(info :: term(), conn :: Conn.t()) :: Conn.reply()
 
+  @doc """
+  `handle_disconnect` allows a callback to handle a disconnect from the websocket server.
+  """
   @callback handle_disconnect(reason :: term(), req :: term(), state :: term()) :: reply :: term()
+
+  @doc """
+  `reply` is a convenience function to reply to a Websocket frame or info. It takes a Websocket frame.
+  """
+  @spec reply(frame :: Frame.t(), conn :: Conn.t()) :: repsonse :: Conn.reply()
+  def reply(frame, conn) do
+    {:reply, frame, conn}
+  end
+
+  @doc """
+  `reply_binary` is a convenience function to reply to a Websocket frame or info, with a binary frame.
+  It will create the binary frame for you, using the provided binary as input.
+  """
+  @spec reply_binary(binary :: binary(), conn :: Conn.t()) :: Conn.reply()
+  def reply_binary(binary, conn) do
+    {:reply, {:binary, binary}, conn}
+  end
+
+  @doc """
+  `reply_text` is a convenience function to reply to a Websocket frame or info, with a text frame.
+  It will create the text frame for you, using the provided text as input.
+  """
+  @spec reply_text(text :: String.t(), conn :: Conn.t()) :: Conn.reply()
+  def reply_text(text, conn) do
+    {:reply, {:text, text}, conn}
+  end
+
+  @doc """
+  `noreply` is a convenience function to not reply to a Websocket frame or info.
+  """
+  @spec noreply(conn :: Conn.t()) :: response :: Conn.reply()
+  def noreply(conn) do
+    {:noreply, conn}
+  end
 end
